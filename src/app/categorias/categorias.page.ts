267b3dad851baad2f4a9c56b7d6c0a458a8fe575
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2019-04-17 12:09:46 -0300

    Criando pagina de produtos

--- src/app/categorias/categorias.page.ts
+++ src/app/categorias/categorias.page.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CategoriaService } from '../services/domain/categoria.service';
 import { CategoriaDTO } from 'src/models/categoria.dto';
 import { API_CONFIG } from 'src/config/api.config';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-categorias',
@@ -14,7 +15,7 @@ export class CategoriasPage {
 
   items: CategoriaDTO[];
 
-  constructor(public categoriaService : CategoriaService) { }
+  constructor(public categoriaService : CategoriaService, public navCtrl: NavController) { }
 
   ngOnInit() {
     // callback com função anônima
@@ -23,5 +24,9 @@ export class CategoriasPage {
         this.items = response;
     },
     error => {});
+  }
+
+  showProdutos(){
+    this.navCtrl.navigateForward('/produtos');
   }
 }
