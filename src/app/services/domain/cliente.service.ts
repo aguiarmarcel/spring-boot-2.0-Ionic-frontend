3545098bba0c2e763a5ee6984bbb6556db58c3e0
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2019-04-23 09:26:53 -0300

    Carregando imagens dos produtos

--- src/app/services/domain/cliente.service.ts
+++ src/app/services/domain/cliente.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ClienteDTO } from 'src/models/cliente.dto';
 import { API_CONFIG } from 'src/config/api.config';
 import { Observable } from 'rxjs';
