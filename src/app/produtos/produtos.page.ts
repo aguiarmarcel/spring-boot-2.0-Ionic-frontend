800094b153b21f2e8b0b354bef2153dc691c2da2
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2019-05-07 17:09:38 -0300

    Infinite scroll

--- src/app/produtos/produtos.page.ts
+++ src/app/produtos/produtos.page.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProdutoDTO } from 'src/models/produto.dto';
-import { NavController, LoadingController} from '@ionic/angular';
+import { NavController, LoadingController, IonInfiniteScroll} from '@ionic/angular';
 import { ProdutoService } from '../services/domain/produto.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { API_CONFIG } from 'src/config/api.config';
-import { LoadedRouterConfig } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-produtos',
@@ -13,7 +12,10 @@ import { LoadedRouterConfig } from '@angular/router/src/config';
 })
 export class ProdutosPage implements OnInit {
 
-  items : ProdutoDTO[];
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  items : ProdutoDTO[] = [];
+  page: number = 0;
 
   constructor(
     public activateRoute: ActivatedRoute, 
@@ -28,19 +30,22 @@ export class ProdutosPage implements OnInit {
       message: "Aguarde..."
     });
     await te.present();
-    this.produtoService.findByCategoria(categoria_id)
+    this.produtoService.findByCategoria(categoria_id, this.page, 10)
     .subscribe(response => {
-      this.items = response['content'];
+      let start = this.items.length;
+      this.items = this.items.concat(response['content']);
+      let end = this.items.length -1;
+      console.log(this.items.length);
       te.dismiss();
-      this.loadImageUrls();
+      this.loadImageUrls(start, end);
     },
     error => {
       this.loudingCtrl.dismiss();
     });
   }
 
-  loadImageUrls(){
-    for (var i = 0; i < this.items.length; i++) {
+  loadImageUrls(start : number, end : number){
+    for (var i = start; i <= end; i++) {
       let item = this.items[i];
       this.produtoService.getSmallImageFromBucket(item.id)
       .subscribe(response =>{
@@ -56,9 +61,22 @@ export class ProdutosPage implements OnInit {
   }
 
   doRefresh(event) {
+    this.page = 0;
+    this.items = [];
     this.ngOnInit();
     setTimeout(() => {
       event.target.complete();
+    }, 1000);
+  }
+
+  doInfinite(scroll){
+    this.page++;
+    this.ngOnInit();
+    setTimeout(() => {
+      this.infiniteScroll.complete();
+      if ( this.items.length == 1000) {
+        this.infiniteScroll.disabled = true;
+      }
     }, 1000);
   }
 }
